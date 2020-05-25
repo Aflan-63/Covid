6b15638ff1c0c0ce9668009680bb9d69ded55e32
Aflan-63 <ifaz.mobile.dev@gmail.com>
2020-05-25 23:03:45 +0600

    Version 3.0.0

--- changeWindow.js
+++ changeWindow.js
@@ -1,5 +1,10 @@
-function changeWindow() {
-    console.log(screen.width);
-    document.location = "../index.html";
-  
+// function changeWindow() {
+//     console.log(screen.width);
+//     document.location = "../index.html";
+// }
+function changeWindow(count) {
+    if (count === 1) {
+        console.log(screen.width);
+        document.location = "../index.html";
+    }
 }
